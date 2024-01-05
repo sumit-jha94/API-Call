6bc45bcda5d110e4abf3b7f6659a2018d811ca69
sumit-jha94 <sumitshine14@gmail.com>
2024-01-06 00:09:04 +0530

    We have changed text on click of button

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function joined() {
     console.log("Clicked")
+    // With getelementbyID we have got the things that needs to be manipulated
+    let title = document.getElementById('title');
+    title.innerText = 'Thanks for joining👌';
+    console.log(title);
 }
